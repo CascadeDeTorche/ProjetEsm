3f9f7fd684483de43e23b2758d9a2b1a391aeca3
CascadeDeTorche <64471244+CascadeDeTorche@users.noreply.github.com>
2020-12-27 14:57:24 +0100

    Précision avec le pi

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
        System.out.println("");
        System.out.println("__________________________________________________");
        System.out.println("");
-       phi = phi*3.14159/180; // on traduit phi en radians pour les fonction cos et sin
+       phi = phi*Math.PI/180; // on traduit phi en radians pour les fonction cos et sin
 
 
            if(Math.sqrt(Math.pow(x,2)+Math.pow(y,2))>a1+a2+d4) // on vérifie que la branche est accessible par la norme
@@ -38,7 +38,7 @@ public class Main {
                double c2 = (Math.pow(Xo2, 2) + Math.pow(Yo2, 2) - Math.pow(a1, 2) - Math.pow(a2, 2)) / (2 * a1 * a2);
                double s2 = Math.sqrt(1 - Math.pow(c2, 2));
                double theta2 = Math.atan2(s2, c2);
-               theta2 = theta2 * 180 / 3.14159;
+               theta2 = theta2 * 180 / Math.PI;
 
                phi = phi * 180 / 3.14159; //on remet phi en degre
 
@@ -52,14 +52,14 @@ public class Main {
                {
 
                    phi = phi + 1;
-                   phi = phi * 3.14159 / 180;
+                   phi = phi * Math.PI / 180;
                    Xo2 = x - d4 * Math.sin(phi); // on définit les variables de la position de O2
                    Yo2 = y + d4 * Math.cos(phi);
                    c2 = (Math.pow(Xo2, 2) + Math.pow(Yo2, 2) - Math.pow(a1, 2) - Math.pow(a2, 2)) / (2 * a1 * a2);
                    s2 = Math.sqrt(1 - Math.pow(c2, 2));
                    theta2 = Math.atan2(s2, c2);
-                   theta2 = theta2 * 180 / 3.14159;
-                   phi = phi * 180 / 3.14159;
+                   theta2 = theta2 * 180 / Math.PI;
+                   phi = phi * 180 / Math.PI;
 
                }
 
@@ -71,7 +71,7 @@ public class Main {
                    double c1 = (Xo2 * (a1 + a2 * c2) + Yo2 * a2 * c2) / (Math.pow(Xo2, 2) + Math.pow(Yo2, 2));
                    double s1 = (Yo2 * (a1 + a2 * c2) - Xo2 * a2 * s2) / (Math.pow(Xo2, 2) + Math.pow(Yo2, 2));
                    double theta1 = Math.atan2(s1, c1);
-                   theta1 = theta1 * 180 / 3.14159;
+                   theta1 = theta1 * 180 / Math.PI;
 
                    //Calcul de Theta3
 
@@ -80,8 +80,8 @@ public class Main {
                    System.out.println("Theta1 vaut : " + theta1);
                    System.out.println("Theta2 vaut : " + theta2);
                    System.out.println("Theta3 vaut : " + theta3);
-                   double xobtenu=d4*Math.sin(phi*3.14159/180)+a2*Math.cos((theta1+theta2)*3.1459/180)+a1*Math.cos(theta1*3.14159/180);
-                   double yobtenu=-d4*Math.cos(phi*3.14159/180)+a2*Math.sin((theta1+theta2)*3.1459/180)+a1*Math.sin(theta1*3.14159/180);
+                   double xobtenu=d4*Math.sin(phi*Math.PI/180)+a2*Math.cos((theta1+theta2)*Math.PI/180)+a1*Math.cos(theta1*Math.PI/180);
+                   double yobtenu=-d4*Math.cos(phi*Math.PI/180)+a2*Math.sin((theta1+theta2)*Math.PI/180)+a1*Math.sin(theta1*Math.PI/180);
                    System.out.println("x : "+xobtenu);
                    System.out.println("y : "+yobtenu);
                    if (phichangé) //si la valeur de phi est changée alors on affiche sa nouvelle valeur
